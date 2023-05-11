0f1ed062e1f5034dc8437330d2b62a1d0d3c1378
Nephilim <nephilim@Belcebux99.eie.ucr.ac.cr>
2023-05-11 13:54:11 -0600

    Code-decoder listo

--- Coder_decoder.py
+++ Coder_decoder.py
@@ -1,20 +1,28 @@
+# Librería utilizada
 import binascii
 
+# Objeto encargado de codificar y decodificar mensaje
 class Bluetooth_sim:
 
     def __init__(self, text_in, text_out):
+        # Texto de entrada "tweet" 
         self.text_in = text_in
+        # Texto de salida en binario
         self.text_out = text_out
 
     def coder(self):
         #Read from txt
         with open(self.text_in, "r") as file:
             cadena = file.read()
-        
-            #print(cadena)
+            print("El mensaje es:",cadena)
         #Coding the text read
             self.coder = bin(int(binascii.hexlify(cadena.encode()), 16))
-            print(self.coder)
+            # La salida tiene "0b" como formato 
+            out = self.coder
+            print("El mensaje codificado con formato de impresión binaria: ", out)
+            # Mensaje binario limpio
+            out_clean = out.replace('b','')
+            print("El mensaje codificado es: ", out_clean)
 
         return self.coder
     
@@ -29,10 +37,22 @@ class Bluetooth_sim:
 text_in = 'tweet_in.txt'
 text_out = 'tweet_out.txt'
 
-sim = Bluetooth_sim(text_in, text_out)
+#########################################
+# Codificación de fuente de información #
+#########################################
 
-sim.coder()
-sim.decoder()
+# Secuencia de bits de información en salida de codificador de fuente
+bf = Bluetooth_sim(text_in, text_out)
+
+############################################
+# Simulación de canal de transmisión ideal #
+############################################
+
+# Se copia información de codificador de salida de fuente
+bfp = bf
+
+bfp.coder()
+bfp.decoder()
 
 
 
